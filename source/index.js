fb122ccf68d39cdcb02564ed3395103f4129a490
Robert Pearce <me@robertwpearce.com>
2019-03-13 10:25:45 +1300

    Change of ownership; added contributors; rm memoization; move component to index file because no need for the fluff

--- source/index.js
+++ source/index.js
@@ -1,1 +1,46 @@
-export { default } from './withResponsive'
+import React, { PureComponent } from 'react'
+
+const withResponsive = (
+  query,
+  identifier,
+  displayName='Responsive'
+) => Comp => {
+  class Responsive extends PureComponent {
+    constructor(...params) {
+      super(...params)
+      this.handleChange = this.handleChange.bind(this)
+      this.state = { isMatch: false }
+    }
+
+    componentDidMount() {
+      this.mql = window.matchMedia(query)
+      this.mql.addListener(this.handleChange)
+
+      this.handleChange()
+    }
+
+    componentWillUnmount() {
+      this.mql.removeListener(this.handleChange)
+    }
+
+    handleChange() {
+      this.setState({ isMatch: this.mql.matches })
+    }
+
+    render() {
+      const { props, state: { isMatch } } = this
+
+      return (
+        <Comp
+          {...props}
+          {...{ [identifier]: isMatch }}
+        />
+      )
+    }
+  }
+  const name = Comp.displayName || Comp.name
+  Responsive.displayName = `${displayName}(${name})`
+  return Responsive
+}
+
+export default withResponsive
